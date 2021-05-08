bcbd78118ad7c6aff6ed800791e6b9e3239e8c01
migheljackson <10728980+migheljackson@users.noreply.github.com>
2021-05-08 03:39:14 -0500

    Added comments to PyBank code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,47 +1,44 @@
+#Import dependencies
 from pandas import read_csv
+
+#Load in csv from resources into a dataframe
 csv_path='Resources/budget_data.csv'
 budgetDF=read_csv(csv_path)
 
-#print(budgetDF.columns)
-#print(budgetDF.count)
+#Find number of entries in the date column
+#Use count() to return number of rows in the Date column
 monthcount=(budgetDF['Date']).count()
-#print(monthcount)
 
-
+#Find net profit for the entire dataset
+#Use sum() to return the sum of all the data in the Profit/Losses column
+#Format the sum to show as dollars 
 netprofit=(budgetDF['Profit/Losses']).sum()
 netprofitformatted="$ {:.2f}".format(netprofit)
 
-#print("${:.2f}".format((budgetDF['Profit/Losses']).sum()))
-
+#Find the average change in profit from month-to-month
+#Use diff() to calulate the difference between a row and the previous row
+#Add Delta as a column to the dataframe and calculate the mean
 monthdelta=(budgetDF['Profit/Losses']).diff()
 budgetDF['Delta']=monthdelta
 avgdelta=budgetDF.Delta.mean()
 avgdeltaformatted="$ {:.2f}".format(avgdelta)
 
-#print("${:.2f}".format(monthdelta.mean()))
-
-#print(monthdelta.max())
-
+#Find the maximum month-to-month profit increase
+#Use argmax() to return the index of the maximum delta
+#Use the iloc[index] to locate the corresponding values in the dataframe for max profit increase
 index=budgetDF.Delta.argmax()
 maxprofitdelta=budgetDF['Delta'].iloc[index]
 maxprofitdeltaformatted="$ {:.2f}".format(maxprofitdelta)
 maxprofitdate=budgetDF['Date'].iloc[index]
-#print(maxprofitdeltaformatted)
-#print(maxprofitdate)
 
+#Find thw lowest month-to-month profit increase
+#Use argmin() to return the index of the minimum delta
 mindex=budgetDF.Delta.argmin()
 minprofitdelta=budgetDF['Delta'].iloc[mindex]
 minprofitdeltaformatted="$ {:.2f}".format(minprofitdelta)
 minprofitdate=budgetDF['Date'].iloc[mindex]
 
-
-#print(budgetDF['Date'].iloc[index])
-
-#maxprofit=budgetDF[budgetDF.Delta == budgetDF.Delta.max()]
-
-
-#minprofit=budgetDF[budgetDF.Delta == budgetDF.Delta.min()]
-#print(minprofit)
+#Write results to a txt file report
 results= open("analysis_result.txt","w+")
 results.write('Financial Analysis\n----------------------\n')
 results.write(f'Total Months: {monthcount}\nTotal Profit: {netprofitformatted}\nAverage Monthly Change: {avgdeltaformatted}\n')
