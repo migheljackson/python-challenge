e5aab2f36b177472be6546994023a44f4485823e
migheljackson <10728980+migheljackson@users.noreply.github.com>
2021-05-08 20:03:01 -0500

    PyPoll commented code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,28 +1,36 @@
+#Add dependencies and read in the csv
 import pandas as pd
 csv_path='Resources/election_data.csv'
 elecdata_DF=pd.read_csv(csv_path)
 
+#Calculate total votes cast by counting VoterID
 totalvotes=elecdata_DF["Voter ID"].count()
 totalvotesform="{:,}".format(totalvotes)
 
+#Calculate the vote counts for each candidate
 votedcandidates_DF=elecdata_DF["Candidate"].value_counts()
 
+#Calculate the percentage of the vote each candidate received
 percentvotes=votedcandidates_DF/totalvotes
 
+#Convert and merge votedcandidate Series and percentvotes Series into a single DataFrame
+#This will allow us to more easily call individual data values for our outputs  
 totvote_df=pd.DataFrame({'Candidate':votedcandidates_DF.index, 'Total Votes':votedcandidates_DF.values})
 percvote_df=pd.DataFrame({'Candidate':percentvotes.index, '% of Vote':percentvotes.values})
 electresult_df=pd.merge(totvote_df,percvote_df,on='Candidate',how='inner')
 
+#Uses argmax to find the index of the row that contains the highest % of Vote and the associated Candidate
 findwindex=electresult_df["% of Vote"].argmax()
 winner=electresult_df['Candidate'].iloc[findwindex]
 
+#Calculates the number of candidates in our data set
 candidatecount=len(electresult_df['Candidate'])
 
 #Generate Terminal outputs for QA
-
 print('Election Results\n---------------\n')
 print(f'Total Votes Cast: {totalvotesform}\n---------------\n')
 
+#For loop generates a formatted output for each candidate
 for x in range(0,candidatecount): 
     name=electresult_df['Candidate'].iloc[x]
     votes=electresult_df['Total Votes'].iloc[x]
